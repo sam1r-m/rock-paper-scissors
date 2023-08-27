57e8927b6c61f7a3e199c96926075703b0f533bb
Samir <samirmohd246@gmail.com>
2023-08-27 13:33:02 +0400

    Implement final win message
    Also cleaned up code and organized code into sections with comments

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// Global variables and initialization
 let computerScore = 0;
 let playerScore = 0;
 
@@ -7,6 +8,15 @@ let score = document.getElementById("score");
 let text = document.createTextNode(playerScore + " - " + computerScore);
 previousScore.appendChild(text);
 score.appendChild(previousScore);
+
+// Helper functions
+
+function capitalizeFirstLetter(word) {
+    let word1 = word.charAt(0).toUpperCase();
+    let word2 = word.slice(1);
+    
+    return (word1 + word2);
+}
 
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
@@ -22,6 +32,53 @@ function getComputerChoice() {
             return "Scissors";
     }
 }
+
+function writeMessage(message) {
+    let element = document.createElement("p");
+    let text = document.createTextNode(message);
+    element.appendChild(text);
+    
+    let div = document.getElementById("results");
+    div.appendChild(element);
+}
+
+function printScore(playerScore, computerScore) {
+    let newScore = document.createElement("h1");
+    let scoreText = document.createTextNode(playerScore + " - " + computerScore);
+    
+    newScore.appendChild(scoreText);
+    score.replaceChild(newScore, previousScore);
+
+    previousScore = newScore;
+}
+
+function printWinner() {
+    let winnerMessage = document.createElement("h2");
+
+    if (playerScore == 5) {
+        let winnerText = document.createTextNode("You win!");
+        winnerMessage.appendChild(winnerText);
+
+    }
+    else if (computerScore == 5) {
+        let winnerText = document.createTextNode("You Lose! The computer beat you!");
+        winnerMessage.appendChild(winnerText);
+
+    }
+
+    results.appendChild(winnerMessage);
+}
+
+// Round functions and event listeners
+
+document.getElementById("rock").addEventListener("click", 
+() => { playRound("Rock", getComputerChoice()); });
+
+document.getElementById("paper").addEventListener("click", 
+() => { playRound("Paper", getComputerChoice()); });
+
+document.getElementById("scissors").addEventListener("click", 
+() => { playRound("Scissors", getComputerChoice()); });
 
 function playRound(playerSelection, computerSelection) {
     if ((playerScore == 5) || (computerScore == 5)) {
@@ -58,83 +115,10 @@ function playRound(playerSelection, computerSelection) {
 
     printScore(playerScore, computerScore);
     writeMessage(result);
+
+    if ((playerScore == 5) || (computerScore == 5)) {
+        printWinner();
+    }
+    
     return winner;
-}
-
-function capitalizeFirstLetter(word) {
-    let word1 = word.charAt(0).toUpperCase();
-    let word2 = word.slice(1);
-    
-    return (word1 + word2);
-}
-
-function game() {
-    let computerScore = 0;
-    let playerScore = 0;
-    let winner;
-
-    /* for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, Paper or Scissors?");
-        winner = (playRound(playerSelection, getComputerChoice()));
-
-        switch(winner) {
-            case "computer":
-                computerScore += 1;
-                break;
-            case "player":
-                playerScore += 1;
-        }
-    } */
-
-    if (playerScore == computerScore) {
-        console.log("It's a Tie!");
-    }
-    else if (playerScore > computerScore) {
-        console.log("You Win!");
-    }
-    else if (computerScore > playerScore) {
-        console.log("You Lose!");
-    }
-
-    console.log("The score is " + playerScore + " - " + computerScore);
-}
-
-document.getElementById("rock").addEventListener("click", 
-() => { 
-    playRound("Rock", getComputerChoice());
-});
-
-document.getElementById("paper").addEventListener("click", 
-() => { 
-    playRound("Paper", getComputerChoice());
-});
-
-document.getElementById("scissors").addEventListener("click", 
-() => { 
-    playRound("Scissors", getComputerChoice());
-});
-
-function writeMessage(message) {
-    let element = document.createElement("p");
-    let text = document.createTextNode(message);
-    element.appendChild(text);
-    
-    let div = document.getElementById("results");
-    div.appendChild(element);
-}
-
-function printScore(playerScore, computerScore) {
-/*     let element = document.createElement("h1");
-    let score = document.getElementById("score");
-    
-    let text = document.createTextNode(playerScore + " - " + computerScore);
-    element.appendChild(text);
-    score.appendChild(element); */
-    let newScore = document.createElement("h1");
-    let scoreText = document.createTextNode(playerScore + " - " + computerScore);
-    newScore.appendChild(scoreText);
-
-    score.replaceChild(newScore, previousScore);
-
-    previousScore = newScore;
 }
